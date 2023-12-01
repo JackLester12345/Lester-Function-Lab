a9fb1f80dd6592128ad43516bc3b68a947a9be66
JackLester12345 <151761901+JackLester12345@users.noreply.github.com>
2023-12-01 13:40:30 -0500

    Add files via upload

---
+++ Functions Lab Jack Lester.py
@@ -0,0 +1,30 @@
+#Jack Lester Pd:8
+
+import math
+
+def getHypotenuse(arg1,arg2):
+    product = 3**2 + 4**2 
+    product = math.sqrt(product)
+    return product
+
+def binaryconverter(arg1):
+    x = 12 
+    
+    for x in range(8):
+        x % 2 = 0
+    
+
+
+
+
+
+    
+
+
+
+if __name__ == "__main__":
+        
+    a,b = 3,4
+    Hypotenuse = getHypotenuse(a,b)
+    print(f"Hypotenuse is: {Hypotenuse}")
+    
